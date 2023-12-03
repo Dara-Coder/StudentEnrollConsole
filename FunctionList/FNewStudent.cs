42707fc09fc7e6179fcb1c23362285eeccfca77b
Bouy Chan Dara <darasmart2020@gmail.com>
2023-12-04 00:19:20 +0700

    update console app with new student crud

--- FunctionList/FNewStudent.cs
+++ FunctionList/FNewStudent.cs
@@ -9,17 +9,18 @@ namespace Student_Enroll_Console.FunctionList
         NewStudentController studentController = new NewStudentController();
         public async Task SeletionNewStudent()
         {
-            Console.WriteLine("1. Create New Student");
+            Console.WriteLine("\n1. Create New Student");
             Console.WriteLine("2. Get All New Students");
             Console.WriteLine("3. Get New Student");
             Console.WriteLine("4. Update New Student");
             Console.WriteLine("5. Delete New Student");
+            Console.WriteLine("6. Exit");
             Console.Write("Choose an option: ");
             int purpose = Convert.ToInt32(Console.ReadLine());
             switch(purpose)
             {
                 case 1:
-                    Console.Write("Input Student Code: ");
+                    Console.Write("\nInput Student Code: ");
                     newStudent.code = Console.ReadLine();
                     Console.Write("Input Student Name: ");
                     newStudent.name = Console.ReadLine();
@@ -32,15 +33,16 @@ namespace Student_Enroll_Console.FunctionList
                     await studentController.SetNewStudentAsync(newStudent);
                     break;
                 case 2:
+                    Console.WriteLine();
                     await studentController.GetAllNewStudent();
                     break;
                 case 3:
-                    Console.Write("Input your student ID: ");
+                    Console.Write("\nInput your student ID: ");
                     int idGet = Convert.ToInt32(Console.ReadLine());
                     await studentController.GetNewStudent(idGet);
                     break;
                 case 4:
-                    Console.Write("Input Student ID: ");
+                    Console.Write("\nInput Student ID: ");
                     newStudent.id = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Input Student Code: ");
                     newStudent.code = Console.ReadLine();
@@ -55,9 +57,13 @@ namespace Student_Enroll_Console.FunctionList
                     await studentController.UpdateNewStudent(newStudent);
                     break;
                 case 5:
-                    Console.Write("Input Student ID: ");
+                    Console.Write("\nInput Student ID: ");
                     int idDelete = Convert.ToInt32(Console.ReadLine());
                     await studentController.DeleteNewStudent(idDelete);
+                    break;
+                case 6:
+                    Console.Clear();
+                    await new OptionsMenu().OptionsMenuList();
                     break;
                 default:
                     break;
